3b49c07d2bf369003833ec4fb3bcc37c8a6553ab
Eugene <zhenyaskryaga@gmail.com>
2025-11-27 13:53:31 +0200

    home work 18

---
+++ main.js
@@ -0,0 +1,51 @@
+console.log('JS #1. Домашнє завдання. Основи JavaScript: Працюємо зі змінними, типами даних');
+
+/*
+ * #1
+ *
+ * Створіть змінні зі значеннями.
+ */
+
+// ім'я змінної: myNum, значення: 10
+// ім'я змінної: myStr, значення: 'some string'
+// ім'я змінної: myBool, значення: true
+// ім'я змінної: myArr, значення: 1, 2, 3, 4, 5
+// ім'я змінної: myObj, значення: first: 'First Name', last: 'Last Name'
+
+const myNum = 10;
+const myStr = 'some string';
+const myBool = true;
+const myArr = [1, 2, 3, 4, 5];
+const myObj = {first: 'First Name', last: 'Last Name'};
+
+console.log(myNum)
+console.log(myStr)
+console.log(myBool)
+console.log(myArr)
+console.log(myObj)
+
+/*
+ * #2
+ *
+ * Відформатуйте ціле число, яке зберігається в змінній myNum, щоб отримати результат з 2 знаками після коми.
+ * Результат збережіть у змінній decimal2.
+ */
+
+
+const decimal2 = myNum.toFixed(2)
+console.log(decimal2)
+
+/*
+ * #3
+ *
+ * Створіть змінну myBigInt і запишіть в неї число 123n (BigInt).
+ * Потім збільште його на 1 та запищіть в цю ж саму змінну.
+ */
+
+let myBigInt = 123n
+console.log(myBigInt)
+myBigInt += 1n
+console.log(myBigInt)
+
+
+
